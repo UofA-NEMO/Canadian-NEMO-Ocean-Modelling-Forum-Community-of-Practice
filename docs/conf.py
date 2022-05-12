441ab4e71f877979352d99fc6a69831c8609c4da
UofA-NEMO <104392485+UofA-NEMO@users.noreply.github.com>
2022-05-11 17:45:38 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
-html_static_path = ['_static', '_static/_UofA', 'Institutions/UofA/_static']
+html_static_path = ['_static', '_static/_UofA', 'Institutions/UofA/Posters']
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
